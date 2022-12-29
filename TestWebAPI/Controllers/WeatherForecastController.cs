630546272f15fbb5199fe493e3a6aac20bf3836a
Divya Dhamotharan <DivyaD@nallas.com>
2022-12-29 12:39:42 +0530

    Fixed commit

--- TestWebAPI/Controllers/WeatherForecastController.cs
+++ TestWebAPI/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace TestWebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase3
+    public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
